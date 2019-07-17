1701b2529827b9fbb4b542fb90e3e91387d41f45
K.I.A Derouiche <kamel.derouiche@gmail.com>
2019-07-17 16:43:04 +0100

    Update nbwquery.go

--- code/nbwquery.go
+++ code/nbwquery.go
@@ -3,10 +3,18 @@ package main
 
 /*
 * K.I.A.Derouiche
-*
+* simple pkgsrc / wip search script using pkgsrc.netbsd.se
 */
 
+import (
+  "fmt"
+  
+  "gopkg.in/urfave/cli.v2"
+)
+
+const version = "1.0"
 const url_website = "http://search.pkgsrc.se/"
 
 func main(){
+  fmt.Println(version)
 }
