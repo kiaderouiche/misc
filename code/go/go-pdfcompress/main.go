7c1b15b03cd17f2bdd28ee5fa010a477fe171780
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2019-08-29 16:58:05 +0100

    write new code for pdf_compress

--- code/go/go-pdfcompress/main.go
+++ code/go/go-pdfcompress/main.go
@@ -1,7 +1,47 @@
 package main
 
+// Author: K.I.A.Derouiche
+// MIT license
+
+/*
+ * Simple Golang wrapper script to use ghostscript function to
+ * compress PDF files.
+ *
+ * Compression levels:
+ *
+ * 0: default
+ * 1: prepress
+ * 2: printer
+ * 3: ebook
+ * 4: screen
+ *
+ *
+ * Dependency: Ghostscript
+ */
+
 import "fmt"
+import "github.com/micro/cli"
+
+//Function to compress PDF via Ghostscript command line interface
+func compress(inputFilePath string, outputFilePath string, power int) {
+	power = 0
+
+	quality := map[int]string{
+		0: "/default",
+		1: "/prepress",
+		2: "/printer",
+		3: "/ebook",
+		4: "/screen",
+	}
+
+	//Temp code!
+	fmt.Println("Compress PDF...")
+
+}
 
 func main() {
+	app := cli.NewApp()
+	app.Name = "go-pdf_compressor"
+	app.Usage = ""
 	fmt.Println("Welcome to pdf-compressor")
 }
