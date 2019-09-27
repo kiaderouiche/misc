de1f9a573239113d57783eed96d548d64ce2b674
K.I.A.Derouiche <kamelderouiche@yahoo.com>
2019-09-27 09:54:12 +0100

    +new book pharaon in Coran

--- config/emacs.d/elpa/cython-mode-20190111.2150/cython-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "cython-mode" "20190111.2150" "Major mode for editing Cython files" 'nil :commit "dfeed35aa487580dc0ac221cd7a1eedd7e467943")
--- config/emacs.d/features/roadmap.md
+++
@@ -1,3 +0,0 @@
-# Ressources
-- https://github.com/Microsoft/vscode-go/wiki/Go-tools-that-the-Go-extension-depends-on
-- https://github.com/magnars/.emacs.d
