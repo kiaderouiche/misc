44dde82c775022e7100382102773b13db16436e4
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2020-11-04 14:54:55 +0100

    using function: form extract

--- code/py/pythonProj3/MailingListStats-0.4.1/pymlstats/extra.py
+++ code/py/pythonProj3/MailingListStats-0.4.1/pymlstats/extra.py
@@ -1,1 +1,13 @@
 import lzma
+
+def extract_file_xz(file_xz):
+    with lzma.open(file_xz) as f:
+        file_content = f.read()
+        decompressed = lzma.decompress(file_content[4:])
+    print(decompressed)
+
+def main():
+    extract_file_xz("./accountsservice-0.6.42.tar.xz")
+
+if __name__ == '__main__':
+    main()
