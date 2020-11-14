c0f3f8aa1d3eeee70b395b0c8696114a4d08a4c6
K.I.A.Derouiche <kamelderouiche@yahoo.com>
2020-11-14 23:46:34 +0100

    new metadata for pkg

---
+++ pythonProj3/RepositoryHandler-0.5/repositoryhandler/_Config.py
@@ -0,0 +1,7 @@
+PACKAGE = 'RepositoryHandler'
+VERSION = '2.1.0'
+
+AUTHOR = 'GSyC/LibreSoft, Universidad Rey Juan Carlos'
+AUTHOR_EMAIL = 'libresoft-tools-devel@lists.morfeo-project.org'
+COPYRIGHT = "Copyright (C) 2004-2011 %s <%s>" % (AUTHOR, AUTHOR_EMAIL)
+DESCRIPTION = "Python library for handling code repositories"
