3553b3bff10844e9e68a8d603f6cfa2ab4fcd238
K.I.A.Derouiche <kamelderouiche@yahoo.com>
2020-11-19 23:32:58 +0100

    CONVERT PY2 CODE to PY3

--- pythonProj3/Bicho-0.9.1/Bicho/info.py
+++
@@ -1,7 +0,0 @@
-PACKAGE = "bicho"
-VERSION = "0.9"
-
-AUTHOR = "GSyC/LibreSoft, Universidad Rey Juan Carlos"
-AUTHOR_EMAIL = "libresoft-tools-devel@lists.morfeo-project.org"
-COPYRIGHT = "Copyright (C) 2007-2011 GSyC/LibreSoft, Universidad Rey Juan Carlos"
-DESCRIPTION = "An analysis tool for issue tracking systems"
