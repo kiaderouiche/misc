53ae096e1ec92c60a667496e5a9c3c2c25a686de
K.I.A.Derouiche <kamelderouiche@yahoo.com>
2020-11-22 21:47:12 +0100

    set Bicho/UserManual.txt

--- pythonProj3/Bicho-2.0/Bicho/info.py
+++ pythonProj3/Bicho-2.0/Bicho/info.py
@@ -3,5 +3,5 @@ VERSION = "2.0"
 
 AUTHOR = "GSyC/LibreSoft, Universidad Rey Juan Carlos"
 AUTHOR_EMAIL = "libresoft-tools-devel@lists.morfeo-project.org"
-COPYRIGHT = "Copyright (C) 2007-2011 GSyC/LibreSoft, Universidad Rey Juan Carlos"
+COPYRIGHT = "Copyright (C) 2007-2020 GSyC/LibreSoft, Universidad Rey Juan Carlos"
 DESCRIPTION = "An analysis tool for issue tracking systems"
